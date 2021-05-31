e1febe7e75ef9bbf0d444a846c954ea4394c9ec2
aleksandra-jpg <aleksandra-jpg@users.noreply.github.com>
2021-06-01 01:26:14 +0200

    new

--- unit5/unit5/wsgi.py
+++ unit5/unit5/wsgi.py
@@ -2,8 +2,9 @@ import sys
 sys.path.insert(0, '/unit5/unit5/unit5_webapp.py')
 import importlib
 
-moduleName = input('unit5_webapp')
-importlib.import_module(moduleName)
+scriptpath = "unit5/unit5/unit5_webapp.py"
+sys.path.append(os.path.abspath(scriptpath))
+import unit5_webapp
 
 from unit5_webapp import app as application
 if __name__ == "__main__":
