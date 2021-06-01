0f1c0910300abb3f8724c887840eea699d253c25
aleksandra-jpg <aleksandra-jpg@users.noreply.github.com>
2021-06-01 02:09:07 +0200

    new

--- unit5/unit5/wsgi.py
+++ unit5/unit5/wsgi.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.insert(0, '/unit5/unit5/unit5_webapp.py')
 
-from unit5_webapp import app, server
+from unit5.unit5.unit5_webapp import app, server
 server = app.server 
 
 if __name__ == "__main__":
