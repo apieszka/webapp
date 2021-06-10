7da4cd6e79d3aa563c6f0c35846763229384079c
ZofiaSchneider <zofia392@gmail.com>
2021-06-10 17:20:14 +0200

    poprawki

--- unit5/unit5/unit5_webapp.py
+++ unit5/unit5/unit5_webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import statistics
+from sqlalchemy import func
 import os
 
 
